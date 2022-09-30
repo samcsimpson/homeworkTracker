8067caeb2918f02f3622d13de4e6d5fe68124995
Sam <samsimpson123_31@hotmail.com>
2022-09-30 21:49:30 +0100

    first commit

---
+++ controllers/comments.js
@@ -0,0 +1,27 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment")
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      console.log(1)
+      console.log(req)
+      console.log(2)
+      console.log(req.file)
+      console.log(3)
+      console.log(req.file.path)
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      await Comment.create({
+        comment: req.body.comment,
+        post: req.params.id,
+        cloudinaryId: result.public_id,
+
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
---
+++ routes/comments.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const CommentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+router.post("/createComment/:id", upload.single("file"), CommentsController.createComment);
+
+module.exports = router;
